d4ee6d0771babbea32ac9f5cd3fe9cc70c0adf8e
kygstar77-creator <kygstar77@gmail.com>
2026-06-09 15:45:40 +0900

    Add salary growth question override

--- src/firemap-v2/financialAssetStepOverride.js
+++ src/firemap-v2/financialAssetStepOverride.js
@@ -1,7 +1,12 @@
+import { defaultInputs } from '../utils/retirementSimulator.js';
 import { questions } from './data.js';
 
-const q = questions.find((item) => item.key === 'financialAsset');
-if (q) {
-  q.step = 5000000;
-  q.unit = '500만 원 단위';
+defaultInputs.salaryGrowthRate ??= 0;
+
+const asset = questions.find((item) => item.key === 'financialAsset');
+if (asset) {
+  asset.step = 5000000;
+  asset.unit = '500만 원 단위';
 }
+
+if (!questions.some((item) => item.key === 'salaryGrowthRate'))

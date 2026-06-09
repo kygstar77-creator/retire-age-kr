4d0f9c089ce96958d1f0ad7cfc6b914cd0766f83
kygstar77-creator <kygstar77@gmail.com>
2026-06-09 15:49:44 +0900

    Restore financial asset override

--- src/firemap-v2/financialAssetStepOverride.js
+++ src/firemap-v2/financialAssetStepOverride.js
@@ -1,12 +1,7 @@
-import { defaultInputs } from '../utils/retirementSimulator.js';
 import { questions } from './data.js';
 
-defaultInputs.salaryGrowthRate ??= 0;
-
-const asset = questions.find((item) => item.key === 'financialAsset');
-if (asset) {
-  asset.step = 5000000;
-  asset.unit = '500만 원 단위';
+const q = questions.find((item) => item.key === 'financialAsset');
+if (q) {
+  q.step = 5000000;
+  q.unit = '500만 원 단위';
 }
-
-if (!questions.some((item) => item.key === 'salaryGrowthRate'))
